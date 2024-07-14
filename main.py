26f5dd94da071a315ed28265a04606778ce05519
Isaac <isaac.vazquez@iteso.mx>
2024-07-13 22:42:06 -0600

    added music and classes

--- main.py
+++ main.py
@@ -1,13 +1,105 @@
 import pygame
 from sys import exit
-from random import randint
+from random import randint, choice
 
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("graphics/player/player-run-1.png").convert_alpha()
-        self.rect = self.image.get_rect(midbottom =(200, 290))
+
+        player_run_1 = pygame.image.load("graphics/player/player-run-1.png").convert_alpha()
+        player_run_2 = pygame.image.load("graphics/player/player-run-2.png").convert_alpha()
+        player_run_3 = pygame.image.load("graphics/player/player-run-3.png").convert_alpha()
+        player_run_4 = pygame.image.load("graphics/player/player-run-4.png").convert_alpha()
+        player_run_5 = pygame.image.load("graphics/player/player-run-5.png").convert_alpha()
+        player_run_6 = pygame.image.load("graphics/player/player-run-6.png").convert_alpha()
+        self.player_run = [player_run_1, player_run_2, player_run_3, player_run_4, player_run_5, player_run_6]
+        self.player_run_index = 0
+
+        player_jump_1 = pygame.image.load("graphics/player/player-jump-1.png").convert_alpha()
+        player_jump_2 = pygame.image.load("graphics/player/player-jump-2.png").convert_alpha()
+        self.player_jump = [player_jump_1, player_jump_2]
+        self.jump_frame_count = 0
+
+        self.image = self.player_run[self.player_run_index]
+        self.rect = self.image.get_rect(midbottom =(150, 290))
+        self.gravity = 0
+
+        self.jump_sound = pygame.mixer.Sound("audio/audio_jump.mp3")
+        self.jump_sound.set_volume(0.2)
+
+    def player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE] and self.rect.bottom >= 290:
+            self.gravity = -20
+            self.jump_sound.play()
+
+    def apply_gravity(self):
+        self.gravity += 1
+        self.rect.y += self.gravity
+        if self.rect.bottom >= 290:
+            self.rect.bottom = 290
+
+    def animation_state(self):
+        if self.rect.bottom < 290:
+            self.jump_frame_count += 1
+            if self.jump_frame_count % 60 < 20:
+                self.image = self.player_jump[0]
+            else:
+                self.image = self.player_jump[1]
+        else:
+            self.player_run_index += 0.2
+            if self.player_run_index >= len(self.player_run): self.player_run_index = 0
+            self.image = self.player_run[int(self.player_run_index)]
+
+    def update(self):
+        self.player_input()
+        self.apply_gravity()
+        self.animation_state()
+
+class Obstacle(pygame.sprite.Sprite):
+
+    def __init__(self, type):
+        super().__init__()
+
+        if type == "eagle":
+            eagle1 = pygame.image.load("graphics/eagle/eagle-attack-1.png").convert_alpha()
+            eagle2 = pygame.image.load("graphics/eagle/eagle-attack-2.png").convert_alpha()
+            eagle3 = pygame.image.load("graphics/eagle/eagle-attack-3.png").convert_alpha()
+            eagle4 = pygame.image.load("graphics/eagle/eagle-attack-4.png").convert_alpha()
+            self.frames = [eagle1, eagle2, eagle3, eagle4]
+            y_pos = randint(150,190)
+        else:
+            mushroom1 = pygame.image.load("graphics/mushroom/mushroom1.png").convert_alpha()
+            mushroom2 = pygame.image.load("graphics/mushroom/mushroom2.png").convert_alpha()
+            mushroom3 = pygame.image.load("graphics/mushroom/mushroom3.png").convert_alpha()
+            mushroom4 = pygame.image.load("graphics/mushroom/mushroom4.png").convert_alpha()
+            mushroom5 = pygame.image.load("graphics/mushroom/mushroom5.png").convert_alpha()
+            mushroom6 = pygame.image.load("graphics/mushroom/mushroom6.png").convert_alpha()
+            mushroom7 = pygame.image.load("graphics/mushroom/mushroom7.png").convert_alpha()
+            mushroom8 = pygame.image.load("graphics/mushroom/mushroom8.png").convert_alpha()
+            mushroom9 = pygame.image.load("graphics/mushroom/mushroom9.png").convert_alpha()
+            self.frames = [mushroom1, mushroom2, mushroom3, mushroom4, mushroom5, mushroom6, mushroom7, mushroom8, mushroom9]
+            y_pos = 293
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900, 1100),y_pos))
+
+    def animation_state(self):
+        self.animation_index += 0.2
+        if self.animation_index >= len(self.frames): self.animation_index = 0
+        self.image = self.frames[int(self.animation_index)]
+
+    def update(self):
+        self.animation_state()
+        self.rect.x -= 5
+        self.destroy()
+
+    def destroy(self):
+        if self.rect.x <= -100:
+            self.kill()
+
 
 # Score function
 def display_score():
@@ -32,11 +124,12 @@ def obstacle_movement(obstacle_list):
     else: return []
 
 # obstacle collsions
-def collisions(player, obstacles):
-    if obstacles:
-        for obstacle_rect in obstacles:
-            if player.colliderect(obstacle_rect): return False
-    return True
+def collisions():
+    if pygame.sprite.spritecollide(player.sprite, obstacle_group, False):
+        obstacle_group.empty()
+        return False
+    else: return True
+
 
 # player animation
 def player_animation():
@@ -71,10 +164,17 @@ game_active = False
 # Time and score
 start_time = 0
 score = 0
+# Music
+bg_music = pygame.mixer.Sound("audio/music.mp3")
+bg_music.play(loops = -1)
+bg_music.set_volume(0.2)
 
-
+#Player
 player = pygame.sprite.GroupSingle()
 player.add(Player())
+
+#Obstacles
+obstacle_group = pygame.sprite.Group()
 
 
 # Create a surface
@@ -176,10 +276,11 @@ while True:
                     player_gravity = -20
 
             if event.type == obstacle_timer:
-                if randint(0,2):
-                    obstacle_rectangle_list.append(npc1_surface.get_rect(midbottom = (randint(900,1100), 293)))
-                else:
-                    obstacle_rectangle_list.append(npc2_surface.get_rect(midbottom = (randint(900,1100), randint(150, 190))))
+                obstacle_group.add(Obstacle(choice(["eagle","mushroom", "mushroom", "mushroom"])))
+                #if randint(0,2):
+                #    obstacle_rectangle_list.append(npc1_surface.get_rect(midbottom = (randint(900,1100), 293)))
+                #else:
+                #    obstacle_rectangle_list.append(npc2_surface.get_rect(midbottom = (randint(900,1100), randint(150, 190))))
 
             if event.type == npc1_animation_timer:
                 npc1_frame_index += 1
@@ -208,19 +309,25 @@ while True:
         score = display_score()
 
         # player
-        player_gravity += 1
-        player_rectangle.bottom += player_gravity
-        if player_rectangle.bottom >= 290: player_rectangle.bottom = 290
-        player_animation()
-        screen.blit(player_surface,player_rectangle)
+        #player_gravity += 1
+        #player_rectangle.bottom += player_gravity
+        #if player_rectangle.bottom >= 290: player_rectangle.bottom = 290
+        #player_animation()
+        #screen.blit(player_surface,player_rectangle)
+
         player.draw(screen)
+        player.update()
+
+        obstacle_group.draw(screen)
+        obstacle_group.update()
 
 
         #obstacle movement
-        obstacle_rectangle_list = obstacle_movement(obstacle_rectangle_list)
+        #obstacle_rectangle_list = obstacle_movement(obstacle_rectangle_list)
 
-        # enemy collision
-        game_active = collisions(player_rectangle, obstacle_rectangle_list)
+        # collision
+        game_active = collisions()
+        #game_active = collisions(player_rectangle, obstacle_rectangle_list)
 
     else:
         screen.fill("#88b07b")
